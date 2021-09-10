bae349e1c09758a6c82f0ee2670d6ba31a641137
worldtechaws <worldtechaws2021@gmail.com>
2021-09-10 14:36:08 -0500

    primer comit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_gestion_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UsuarioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string query = @"
+                select usuario_id,usuario_nombre,usuario_apellido,username,usuario_email,usuario_celular,
+                usuario_imagen,gaula.gaula_nombre,rol.rol_nombre,usuario.fecha_actualizacion,usuario_identificacion,password
+                from usuario
+                join rol
+                on usuario.rol_id=rol.rol_id
+                join gaula
+                on usuario.gaula_id=gaula.gaula_id
+                where usuario.estado_id = 1 
+            ";
+
+            string sqlDataSource = _configuration.GetConnectionString("Conexion");
+            DataTable table = new DataTable();
+            NpgsqlDataReader myReader;
+
+            try
+            {
+                using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok("Error: " + ex);
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_gestion_core.Models
+{
+    public class Usuario
+    {
+        public int usuario_id { get; set; }
+        public string usuario_nombre { get; set; }
+        public string usuario_apellido { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string usuario_email { get; set; }
+        public string usuario_celular { get; set; }
+        public string usuario_imagen { get; set; }
+        public int gaula_id { get; set; }
+        public int estado_id { get; set; }
+        public int rol_id { get; set; }
+        public DateTime fecha_actualizacion { get; set; }
+        public string usuario_identificacion { get; set; }
+    }
+}
